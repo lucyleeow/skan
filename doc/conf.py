890efdf26558d0217cea0b9c217a1533adb85168
lucy liu <jliu176@gmail.com>
2022-05-26 11:35:19 +1000

    fix switcher version match

--- doc/conf.py
+++ doc/conf.py
@@ -146,6 +146,9 @@ html_theme_options = {
 #     'globaltoc_includehidden': False,
 }
 
+if 'dev' in version:
+    html_theme_options["switcher"]["version_match"] = "dev"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
